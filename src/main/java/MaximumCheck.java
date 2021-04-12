bc544d77d33c793c0eb7de936549655436cc2272
MalavikaAjit <malavikaajit9@gmail.com>
2021-04-12 19:07:35 +0530

    REFACTOR: refactored to create generic class to take 3 variables of generic type

--- src/main/java/MaximumCheck.java
+++ src/main/java/MaximumCheck.java
@@ -1,7 +1,15 @@
 
-public class MaximumCheck{
+public class MaximumCheck <T extends Comparable>{
 
-    public static <T extends Comparable<T>> T testMax(T x, T y ,T z){
+    T x,y,z;
+    T max;
+
+    public MaximumCheck(T x,T y,T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public static < T extends Comparable <T>> T testMax(T x, T y ,T z){
         T max = x;
         if(y.compareTo(max) > 0)
             max = y;
@@ -9,11 +17,18 @@ public class MaximumCheck{
             max = z;
         return max;
     }
+    public T testMax(){
+        return (T) MaximumCheck.testMax(x,y,z);
+    }
 
     public static void main(String[] args) {
-        System.out.println(MaximumCheck.testMax(3,2,1));
-        System.out.println(MaximumCheck.testMax(3.2, 2.2,1.2));
-        System.out.println(MaximumCheck.testMax("peach","banana","apple"));
+       Integer xInt=3, yInt=2,zInt=1;
+       Float xF1=3.2f, yF2=2.2f, zF3=1.2f;
+       String xStr="peach",yStr="banana",zStr="apple";
 
+       MaximumCheck.testMax(xStr,yStr,zStr);
+       MaximumCheck.testMax(xInt,yInt,zInt);
+       MaximumCheck.testMax(xF1,yF2,zF3);
+       // maximumCheck.testMax(xStr,yStr,zStr);
     }
 }
--- src/test/java/MaximumCheckTest.java
+++ src/test/java/MaximumCheckTest.java
@@ -4,53 +4,61 @@ import org.junit.jupiter.api.Test;
 
 public class MaximumCheckTest {
 
-    MaximumCheck maximumCheck = new MaximumCheck();
+   // MaximumCheck maximumCheck = new MaximumCheck();
 
     @Test
     public void givenIntegers_whenMaxAtFirst_ReturnSame() {
-        Integer result = maximumCheck.testMax(3,1,2);
+        MaximumCheck<Integer> maximumCheck = new MaximumCheck<Integer>(3,2,1);
+        Integer result = maximumCheck.testMax();
         Assertions.assertEquals(3, result);
     }
 
     @Test
     public void givenIntegers_whenMaxAtSecond_ReturnSame() {
-        Integer result = maximumCheck.testMax(2,3,1);
+        MaximumCheck<Integer> maximumCheck = new MaximumCheck<Integer>(2,3,1);
+        Integer result = maximumCheck.testMax();
         Assertions.assertEquals(3, result);
     }
     @Test
     public void givenIntegers_whenMaxAtThird_ReturnSame() {
-        Integer result = maximumCheck.testMax(1,2,3);
+        MaximumCheck<Integer> maximumCheck = new MaximumCheck<Integer>(1,2,3);
+        Integer result = maximumCheck.testMax();
         Assertions.assertEquals(3, result);
     }
     @Test
     public void givenFloats_whenMaxAtFirst_ReturnSame() {
-       Float result = maximumCheck.testMax(3.2f,1.2f,2.2f);
+        MaximumCheck<Float> maximumCheck = new MaximumCheck<Float>(3.2f,1.2f,2.2f);
+        Float result = maximumCheck.testMax();
         Assertions.assertEquals(3.2f, result);
     }
     @Test
     public void givenFloats_whenMaxAtSecond_ReturnSame() {
-        Float result = maximumCheck.testMax(2.2f,3.2f,1.2f);
+        MaximumCheck<Float> maximumCheck = new MaximumCheck<Float>(2.2f,3.2f,1.2f);
+        Float result = maximumCheck.testMax();
         Assertions.assertEquals(3.2f, result);
     }
     @Test
     public void givenFloats_whenMaxAtThird_ReturnSame() {
-        Float result = maximumCheck.testMax(1.2f,2.2f,3.2f);
+        MaximumCheck<Float> maximumCheck = new MaximumCheck<Float>(1.2f,2.2f,3.2f);
+        Float result = maximumCheck.testMax();
         Assertions.assertEquals(3.2f, result);
     }
     @Test
     public void givenStrings_whenMaxAtFirst_ReturnSame() {
-       String result = maximumCheck.testMax("peach","apple","banana");
+        MaximumCheck<String> maximumCheck = new MaximumCheck<String>("peach","apple","banana");
+        String result = maximumCheck.testMax();
         Assertions.assertEquals("peach", result);
     }
     @Test
     public void givenStrings_whenMaxAtSecond_ReturnSame() {
-       String result = maximumCheck.testMax("apple","peach","banana");
+        MaximumCheck<String> maximumCheck = new MaximumCheck<String>("apple","peach","banana");
+        String result = maximumCheck.testMax();
         Assertions.assertEquals("peach", result);
     }
     @Test
     public void givenStrings_whenMaxAtThird_ReturnSame() {
-        String result = maximumCheck.testMax("peach","banana","peach");
+        MaximumCheck<String> maximumCheck = new MaximumCheck<String>("peach","banana","peach");
+        String result = maximumCheck.testMax();
         Assertions.assertEquals("peach", result);
     }
-
 }
